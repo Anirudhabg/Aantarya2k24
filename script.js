d612715e6e4b10448516f8b903427fb75f35a657
Akash C Bhat <akashcbhat2001@gmail.com>
2024-01-01 21:25:43 +0530

    added mandala and all

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let ugIdx = 0;
 let pgIdx = 0;
+let ugNIdx = 0;
+let pgNIdx = 0;
 
 const ugList = [
   "MOOK SAMVAD",
@@ -8,10 +10,18 @@ const ugList = [
   "Pranshnothar",
   "Vadah",
   "Nrityam",
+  "Chayachitr",
   "Khelan",
   "Nidhisangrah",
   "Pracharan",
 ];
+
+
+const ugListNorm = [
+  "DUMB CHARADES", "CODING", "WEB DESIGNING", "QUIZ", "DEBATE", "DANCE", "PHOTOGRAPHY", "GAMING", "TREASURE HUNT", "PRODUCT LAUNCH"
+]
+
+
 
 const pgList = [
   "IT Pramukh",
@@ -27,24 +37,36 @@ const pgList = [
   "Chitr",
 ];
 
-const ugCard = document.querySelector("#ug-card .inside-card-h3");
-const pgCard = document.querySelector("#pg-card .inside-card-h3");
+const pgListNorm = [
+  "IT MANAGER","CODING","WEB DESIGNING","QUIZ","DEBATE","PHOTOGRAPHY","DANCE","GAMING","TREASURE HUNT","PRODUCT LAUNCH","DESIGNING"
+]
 
+const ugCard = document.querySelector("#ug-card .inside-card-h3");
+const ugNCard = document.querySelector("#ug-card .inside-card-normal-h3");
+
+const pgCard = document.querySelector("#pg-card .inside-card-h3");
+const pgNCard = document.querySelector("#pg-card .inside-card-normal-h3");
 
 function showNextCard() {
   ugIdx = (ugIdx - 1 + ugList.length) % ugList.length;
+  ugNIdx = (ugNIdx - 1 + ugListNorm.length) % ugListNorm.length;
+
   ugCard.innerText = ugList[ugIdx];
+  ugNCard.innerText = ugListNorm[ugNIdx];
 }
 
 function showPreviousCard() {
   ugIdx = (ugIdx + 1) % ugList.length;
+  ugNIdx = (ugNIdx + 1) % ugListNorm.length;
   ugCard.innerText = ugList[ugIdx];
+  ugNCard.innerText = ugListNorm[ugNIdx];
 }
 
 const ugRegisterButton = document.querySelector("#ug-card");
 
 ugRegisterButton.addEventListener("click", () => {
   console.log(ugList[ugIdx]);
+  console.log(ugListNorm[ugNIdx]);
 });
 
 
@@ -52,12 +74,16 @@ ugRegisterButton.addEventListener("click", () => {
 //pg cards
 function showNextCard2() {
   pgIdx = (pgIdx - 1 + pgList.length) % pgList.length;
+  pgNIdx = (pgNIdx - 1 + pgListNorm.length) % pgListNorm.length;
   pgCard.innerText = pgList[pgIdx];
+  pgNCard.innerText = pgListNorm[pgNIdx];
 }
 
 function showPreviousCard2() {
   pgIdx = (pgIdx + 1) % pgList.length;
+  pgNIdx = (pgNIdx + 1) % pgListNorm.length;
   pgCard.innerText = pgList[pgIdx];
+  pgNCard.innerText = pgListNorm[pgNIdx];
 }
 
 const pgRegisterButton = document.querySelector("#pg-card");
@@ -65,4 +91,5 @@ console.log(pgRegisterButton);
 
 pgRegisterButton.addEventListener("click", () => {
   console.log(pgList[pgIdx]);
+  console.log(pgListNorm[pgNIdx]);
 });

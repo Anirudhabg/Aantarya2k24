f3985896c19b8eda7125837e8d24ee7fb0141de5
Mahammad Afnan <muhammedafnan8184@gmail.com>
2024-01-12 21:44:08 +0530

    navigation to dashborad fixed. fixed inital load time on the server.

---
+++ js/dborad.js
@@ -0,0 +1,32 @@
+// const API_URL = "http://localhost:3000";
+const API_URL = "https://mca-fest.onrender.com";
+
+const token = localStorage.getItem("jwtToken");
+const teamId =  localStorage.getItem("teamId");
+
+const burgerMenu = document.querySelector(".burger-click");
+const navLinks = document.querySelector(".left-fixed-nav");
+
+burgerMenu.addEventListener("click", function () {
+  navLinks.classList.toggle("show");
+});
+
+
+function validateNumericInput(inputElement) {
+  inputElement.value = inputElement.value.replace(/\D/g, "");
+}
+
+
+
+const removeToken = () => {
+  localStorage.removeItem("jwtToken");
+  localStorage.removeItem("teamId");
+};
+
+
+const logOutBtn = document.querySelector("#logoutBtn");
+
+logOutBtn.onclick = () => {
+  removeToken();
+  window.location.href = "/pages/login.html";
+};

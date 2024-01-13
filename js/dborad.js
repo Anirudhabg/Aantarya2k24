9ecafbd76085adc32217dae5788a4b8fd35d6e92
Akash C Bhat <akashcbhat2001@gmail.com>
2024-01-13 19:21:14 +0530

    added counter

--- js/dborad.js
+++ js/dborad.js
@@ -2,7 +2,7 @@
 const API_URL = "https://mca-fest.onrender.com";
 
 const token = localStorage.getItem("jwtToken");
-const teamId =  localStorage.getItem("teamId");
+const teamId = localStorage.getItem("teamId");
 
 const burgerMenu = document.querySelector(".burger-click");
 const navLinks = document.querySelector(".left-fixed-nav");
@@ -62,7 +62,7 @@ paymentBtn.onclick = () => {
   accomodationSec.classList.add('hide')
   settingSec.classList.add('hide')
   contactSec.classList.add('hide')
-  
+
 }
 
 accomodationBtn.onclick = () => {
@@ -89,3 +89,33 @@ contactBtn.onclick = () => {
   contactSec.classList.remove('hide')
 }
 
+
+
+// Set the date we're counting down to
+var countDownDate = new Date("February 15, 2024 23:59:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);

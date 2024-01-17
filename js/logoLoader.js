621a2d8ae310de3f67a38892b964619e03f94ab6
Mahammad Afnan <muhammedafnan8184@gmail.com>
2024-01-17 22:30:40 +0530

    test commit

---
+++ js/logoLoader.js
@@ -0,0 +1,8 @@
+
+const loaderContainer = document.querySelector('.loader-container');
+
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        loaderContainer.style.display = 'none';
+    }, 3000);
+});

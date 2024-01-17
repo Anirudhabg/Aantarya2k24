52a6605e278cf416aac2c9e582cd5ed61697b4a5
Mahammad Afnan <muhammedafnan8184@gmail.com>
2024-01-17 22:38:40 +0530

    added logoLoader.js

--- js/logoLoader.js
+++ js/logoLoader.js
@@ -5,10 +5,5 @@ window.addEventListener('load', () => {
     setTimeout(() => {
         loaderContainer.style.display = 'none';
     }, 3000);
-
-const loaderContainer = document.querySelector('.loader-container');
-
-window.addEventListener('load', () => {
-    setTimeout(() => {
-        loaderContainer.style.display = 'none';
-    }, 2000);
+    
+});

64793ecc15d2ff1a99bdbeec59eb59dfedd09b17
Akash C Bhat <akashcbhat2001@gmail.com>
2024-01-18 07:43:58 +0530

    trying loader

--- js/logoLoader.js
+++ js/logoLoader.js
@@ -2,8 +2,14 @@
 const loaderContainer = document.querySelector('.loader-container');
 
 window.addEventListener('load', () => {
+
     setTimeout(() => {
-        loaderContainer.style.display = 'none';
+        // loaderContainer.style.display = 'none';
+        loaderContainer.classList.add("loader-hidden");
     }, 3000);
+
+    // setTimeout(() => {
+    //     loaderContainer.style.display = 'none';
+    // }, 3000);
     
 });

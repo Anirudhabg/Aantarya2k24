22d2320e3fcf62c6240beb3b527145d8457d27fa
Akash C Bhat <akashcbhat2001@gmail.com>
2024-01-30 20:55:51 +0530

    it was earlier version dont consider all

--- js/logoLoader.js
+++ js/logoLoader.js
@@ -1,18 +1,20 @@
 const loaderPc = document.querySelector('.loader-pc');
 const loaderMobile = document.querySelector('.loader-mobile');
 const loaderlottie=document.querySelector('.loader-lottie');
-
+const loaderContainer = document.querySelector('.loader-container')
 
 window.addEventListener('load', () => {
     loaderlottie.classList.add("loader-hidden");
-        // loaderContainer.style.display = 'none';
+        loaderContainer.style.display = 'none';
         loaderPc.classList.add("loader-hidden");
         loaderMobile.classList.add("loader-hidden");
    
-
-    // setTimeout(() => {
-    //     loaderContainer.style.display = 'none';
-    // }, 3000);
+        const token = localStorage.getItem("jwtToken");
+        if (!token) {
+          setTimeout(() => {
+              loaderContainer.style.display = 'block';
+          }, 3000);
+        }
 
 })
 /*
